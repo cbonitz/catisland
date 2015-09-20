c811c61520e18e5e0d7ea21bb7cf089d0ca267c8
Christoph Bonitz <ml.christophbonitz@gmail.com>
2015-09-20 19:33:03 +0200

    refactoring using functions

--- kitty.go
+++ kitty.go
@@ -5,6 +5,31 @@ import "os"
 import "io/ioutil"
 import "net/http"
 import "strings"
+import "errors"
+
+// TomcatManager is data type for tomcat manager configuration
+type TomcatManager struct {
+	host     string
+	username string
+	password string
+}
+
+// String functon for TomcatManager
+func (t TomcatManager) String() string {
+	passwordOutput := t.password != ""
+	return fmt.Sprintf("host '%s' username '%s' password? %t\n", t.host, t.username, passwordOutput)
+}
+
+// CreateManager creates a TomcatManager
+func CreateManager(trimmedLine string) (result *TomcatManager, err error) {
+	items := strings.Split(trimmedLine, ";")
+	if len(items) != 3 {
+		return nil, errors.New("lines must be formatted as hostname;user;password, but found " + trimmedLine)
+	}
+	config := TomcatManager{host: items[0] + "manager/text/list", username: items[1], password: items[2]}
+	fmt.Printf("%s\n", config)
+	return &config, nil
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -19,27 +44,24 @@ func main() {
 		panic(err)
 	}
 	lines := strings.Split(string(content), "\n")
-	var hosts []map[string]string
+	var hosts []*TomcatManager
 	for _, line := range lines {
 		trimmed := strings.Trim(line, "\r")
 		if len(trimmed) > 0 {
-			items := strings.Split(trimmed, ";")
-			if len(items) != 3 {
-				fmt.Println("lines must be formatted as hostname;user;password, but found", trimmed)
+			config, err := CreateManager(trimmed)
+			if err != nil {
+				fmt.Println("Error: ", err.Error())
 				os.Exit(1)
+			} else {
+				hosts = append(hosts, config)
 			}
-			config := make(map[string]string)
-			config["host"] = items[0]
-			config["user"] = items[1]
-			config["password"] = items[2]
-			fmt.Printf("host '%s' user '%s' password '%s'\n", config["host"], config["user"], config["password"])
-			hosts = append(hosts, config)
 		}
 	}
+
 	client := &http.Client{}
 	for _, host := range hosts {
-		req, err := http.NewRequest("GET", host["host"], nil)
-		req.SetBasicAuth(host["user"], host["password"])
+		req, err := http.NewRequest("GET", host.host, nil)
+		req.SetBasicAuth(host.username, host.password)
 		resp, err := client.Do(req)
 		if err != nil {
 			fmt.Println("Error on request.", err)
